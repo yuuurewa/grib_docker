017154b6c4640e3554f75488d58531e6de7798f3
yuuurewa <yurewa.vika23@mail.ru>
2024-08-12 16:59:37 +0700

    update unpack_grib.py

--- unpack_grib.py
+++ unpack_grib.py
@@ -1,16 +1,16 @@
 import pygrib
 import os
 
+
+def open_grib(f_name: str) -> None:
+    with pygrib.open(os.path.join('grib_file', f_name)) as grbs:
+        for grb in grbs:
+            print(grb)
+        print()
+
+
 if __name__ == '__main__':
-    print('Search for files in the directory: ' + os.getcwd() +'\n')
-    for dirpath, _, filenames in os.walk(os.getcwd()):
-        for filename in filenames:
-            extension = os.path.splitext(filename)[1].lower()
-            if extension == '.grib' or extension == '.grib2':
-                grbs = pygrib.open(os.path.join(dirpath, filename))
-                grbs.seek(0)
-                for grb in grbs:
-                    print(grb)
-                print()
-            else:
-                continue
+    print('Search for files in the directory...\n')
+    for filename in os.listdir('grib_file'):
+        if filename.lower().endswith(('.grib', '.grib2')):
+            open_grib(filename)
