aed354aa04e4f007cae28966e5179cb5ac49179c
yuuurewa <yurewa.vika23@mail.ru>
2024-08-13 15:02:06 +0700

    upgrade unpack_grib.py [2]

--- unpack_grib.py
+++ unpack_grib.py
@@ -4,23 +4,26 @@ import pygrib
 PATH_TO_GRIB = '/grib_docker/grib_file'
 
 
-def check_extension(f_name: str) -> str:
-    if f_name.lower().endswith(('.grib', '.grib2')):
-        return f_name
+def search_grib() -> list[str]:
+    grib_files = []
+    for filename in os.listdir(PATH_TO_GRIB):
+        if filename.lower().endswith(('.grib', '.grib2')):
+            grib_files.append(filename)
+    return grib_files
 
 
-def unpack_file(f_name: str) -> None:
-    with pygrib.open(os.path.join(PATH_TO_GRIB, f_name)) as grbs:
-        for grb in grbs:
-            print(grb)
-        print(50 * '-')
+def unpack_file(grib_files: list[str]) -> None:
+    for grib_file in grib_files:
+        print('Unpack file "' + grib_file + '"\n')
+        with pygrib.open(os.path.join(PATH_TO_GRIB, grib_file)) as grbs:
+            for grb in grbs:
+                print(grb)
+            print(50 * '-')
 
 
 def main():
-    print('Search for files in the directory...\n')
-    for filename in os.listdir(PATH_TO_GRIB):
-        file_name = check_extension(filename)
-        unpack_file(file_name)
+    grib_files = search_grib()
+    unpack_file(grib_files)
 
 
 if __name__ == '__main__':
