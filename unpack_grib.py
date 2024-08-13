df2131117f11204546a9d88a6099ccaf443d8f4c
yuuurewa <yurewa.vika23@mail.ru>
2024-08-13 12:13:38 +0700

    upgrade unpack_grib.py

--- unpack_grib.py
+++ unpack_grib.py
@@ -1,16 +1,27 @@
-import pygrib
 import os
+import pygrib
+
+PATH_TO_GRIB = '/grib_docker/grib_file'
 
 
-def open_grib(f_name: str) -> None:
-    with pygrib.open(os.path.join('grib_file', f_name)) as grbs:
+def check_extension(f_name: str) -> str:
+    if f_name.lower().endswith(('.grib', '.grib2')):
+        return f_name
+
+
+def unpack_file(f_name: str) -> None:
+    with pygrib.open(os.path.join(PATH_TO_GRIB, f_name)) as grbs:
         for grb in grbs:
             print(grb)
-        print()
+        print(50 * '-')
+
+
+def main():
+    print('Search for files in the directory...\n')
+    for filename in os.listdir(PATH_TO_GRIB):
+        file_name = check_extension(filename)
+        unpack_file(file_name)
 
 
 if __name__ == '__main__':
-    print('Search for files in the directory...\n')
-    for filename in os.listdir('grib_file'):
-        if filename.lower().endswith(('.grib', '.grib2')):
-            open_grib(filename)
+    main()
